105e78bb893778748ee8ffdab4a916707eb2f702
legacу <harveycompanystart@gmail.com>
2025-05-05 12:14:05 +0300

    Update Tmarket.lua

--- Tmarket.lua
+++ Tmarket.lua
@@ -1,6 +1,6 @@
 script_name("Market Price")
 script_author("legacy")
-script_version("3")
+script_version("2")
 
 local ffi = require("ffi")
 local encoding = require("encoding")
@@ -62,7 +62,6 @@ local function checkUpdate()
     local j = decodeJson(response.text)
     if thisScript().version == j.last then return end
 
-    -- Скачиваем новый скрипт
     downloadUrlToFile(j.url, thisScript().path, function(_, status)
         if status == dlstatus.STATUSEX_ENDDOWNLOAD then
             local f = io.open(thisScript().path, "r")
@@ -75,26 +74,6 @@ local function checkUpdate()
             thisScript():reload()
         end
     end)
-
-    -- Скачиваем новый market_price.ini, если файл уже существует
-    local configFile = io.open(configPath, "r")
-    if configFile then
-        configFile:close()
-        os.remove(configPath)  -- Удаляем старый файл
-    end
-
-    -- Добавляем проверку перед скачиванием
-    local function downloadMarketPrice()
-        downloadUrlToFile(j.market_price_ini_url, configPath, function(_, status)
-            if status == dlstatus.STATUSEX_ENDDOWNLOAD then
-                print("Market price ini file updated.")
-            elseif status == dlstatus.STATUSEX_ERROR then
-                print("Error downloading market_price.ini")
-            end
-        end)
-    end
-    
-    downloadMarketPrice()
 end
 
 local function checkNick()
